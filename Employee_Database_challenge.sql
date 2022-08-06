244926f9995ce0ad925fe797521c28a6ee152d52
guevarch <107594143+guevarch@users.noreply.github.com>
2022-08-05 22:42:16 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Retirement Titles
 SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
 INTO retirement_titles
 FROM employees as e
@@ -6,7 +7,7 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no ASC
 
-
+-- Unique Titles
 SELECT DISTINCT rt.emp_no, rt.first_name, rt.last_name, rt.title
 INTO unique_titles
 FROM retirement_titles as rt 
@@ -20,7 +21,7 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
 
-
+-- Mentorship Eligibility 
 SELECT e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, ti.title   
 INTO mentorship_eligibility,  
 FROM employees as e
@@ -29,12 +30,40 @@ ON (e.emp_no = de.emp_no )
 INNER JOIN titles as ti
 ON (de.emp_no = ti.emp_no )
 
+------------------------------------------------------------------
+-- Count ages between 1964 - 1970
+SELECT COUNT (e.emp_no)
+INTO bdate1964_1970
+FROM employees as e
+WHERE (e.birth_date BETWEEN '1964-01-01' AND '1970-12-31')
+-- 24853 employees
 
+-- Count ages between 1952 - 1957
+SELECT COUNT (e.emp_no)
+INTO bdate1952_1957
+FROM employees as e
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1957-12-31')
+-- 136299 employees
 
+-- Count ages between 1958 - 1963
+SELECT COUNT (e.emp_no)
+INTO bdate1958_1963
+FROM employees as e
+WHERE (e.birth_date BETWEEN '1958-01-01' AND '1963-12-31')
+-- 138872 employees
 
+--------------- Most impacted department from retirement
 
+Select retirement_info.emp_no, retirement_info.first_name, retirement_info.last_name,
+dept_info.dept_name
+INTO RETIREMENTINFO2
+FROM retirement_info
+INNER JOIN dept_info
+ON (retirement_info.emp_no = dept_info.emp_no)
 
-
+SELECT COUNT(RT.emp_no), RT.dept_name
+FROM RETIREMENTINFO2 AS RT
+GROUP BY RT.dept_name
 
 
 
